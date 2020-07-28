be4e5bc9e4a45d04c14b799b6cbc1c3ef0c1d642
FathimaFairoos <60708729+FathimaFairoos@users.noreply.github.com>
2020-07-28 19:25:54 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,sum;
+    printf("Enter first number:\n");
+    scanf("%d",&num1);
+    printf("Enter second number:\n");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("Result is :%d",sum);
+    return 0;
+}
